16e6ae64f59fc739a3becddf0fcaaf4292f5abb5
lrbn86 <lrbn86@gmail.com>
2020-02-23 17:17:37 -0500

    Used a hack to hide video on mobile using JS

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const aboutme = document.getElementById("about-me");
 arrowbtn.addEventListener("click", function() {
   aboutme.scrollIntoView(true);
 });
+
+if (window.matchMedia('(max-width: 960px)').matches) {
+  const videoElement = document.getElementById("bg-video");
+  videoElement.pause();
+  videoElement.removeAttribute('src');
+  videoElement.load();
+}
